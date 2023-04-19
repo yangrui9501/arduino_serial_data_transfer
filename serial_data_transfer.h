efe8796ae92b0cf26f7729d79e2ff34cb46b16ff
yangrui9501 <yang77567789@gmail.com>
2023-04-19 09:48:19 +0800

    add get_uart_msg()

--- serial_data_transfer.h
+++ serial_data_transfer.h
@@ -33,38 +33,57 @@ public:
      * @param _config Sets data, parity, and stop bits. For example: SERIAL_8N1
      */
     void init(HardwareSerial& _uart_handler, uint32_t _baud_rate, int _config = SERIAL_8N1);
-    
+
     /**
      * @brief Begin serial communication
-     * 
+     *
      */
     void begin();
-    
+
     /**
      * @brief Receive n bytes data
-     * 
+     *
      * @param _dest Destination of data structure
      * @param num_of_bytes_to_receive Number of bytes to receive
-     * @return int 
+     * @return int
      */
     int receive(void* _dest, const int& num_of_bytes_to_receive);
 
     /**
      * @brief Transmit n bytes data from _src
-     * 
+     *
      * @param _src Source data
      * @param num_of_bytes_to_transmit Number of bytes to transmit
-     * @return int 
+     * @return int
      */
     int transmit(void* _src, const int& num_of_bytes_to_transmit);
 
     /**
      * @brief Get the serial interface object
-     * 
-     * @return HardwareSerial& 
+     *
+     * @return HardwareSerial&
      */
     inline HardwareSerial& get_serial_interface() {
         return *pUART;
+    }
+    /**
+     * @brief Get the uart message
+     *
+     * @return const char*
+     */
+    const char* get_uart_msg(void)
+    {
+        switch (uart_data_status)
+        {
+        case DATA_NOT_YET_READY:
+            return "Data not yet ready";
+        case DATA_LOST_BEGIN_BYTE:
+            return "Lost the first begin byte";
+        case DATA_LOST_FINISH_BYTE:
+            return "Lost the second begin byte";
+        case DATA_OK:
+            return "Data OK";
+        }
     }
 protected:
     struct
